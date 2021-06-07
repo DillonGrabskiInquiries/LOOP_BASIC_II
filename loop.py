36e37292ceacfede0c56bd72eaa728aeda91b89a
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-07 17:14:31 -0500

    1

---
+++ loop.py
@@ -0,0 +1,46 @@
+# def countdown(num):
+#     while num >= 0:
+#         print(num)
+#         num -= 1
+
+# countdown(5)
+
+
+
+
+# def print_and_return(list):
+#     print(list[0])
+#     return list[1]
+
+# print_and_return([1, 2])
+
+
+
+# def first_plus_length(list):
+#     print(1 + len(list))
+
+# first_plus_length([1, 2, 3, 4, 5])
+
+
+
+# def values_greater_than_second(list):
+#     secondList = []
+#     total = 0
+#     for i in list:
+#         if list[i] > list[1]:
+#             total += 1
+#             secondList.append(list[i])
+#             print(total)
+#             return secondList
+
+# values_greater_than_second([5,2,3,2,1,4])
+
+
+
+# def length_and_value(num1, num2):
+#     newList = []
+#     len(newList) = num1
+#     newList.append(num2)
+#     return newList
+
+# length_and_value(4, 5)
